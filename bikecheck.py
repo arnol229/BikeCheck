657f8d4955ebaaafeb685468c64c70ef71528fa8
Christian Monsegue <c0m01h9@homeoffice.wal-mart.com>
2018-08-03 22:36:25 -0500

    Finalized navigation

--- bikecheck.py
+++ bikecheck.py
@@ -3,16 +3,24 @@ from guizero import App, Text, TextBox, PushButton, Slider, Picture
 class WelcomeView:
     view_name = 'welcome_view'
 
-    def __init__(self, application, on_enter, history):
+    def __init__(self, application, on_exit_view = None):
         self.app = application
-        self.on_enter = on_enter
+        self.on_exit_view = on_exit_view
         self.header_text = Text(self.app, text="Welcome to BikeCheck", size=40, font="Times New Roman", color="lightblue", grid=[])
         self.button = PushButton(self.app, command=self.on_face_recognition, text="Enter")
-        self.history = history
 
     def on_face_recognition(self):
         self.button.disable()
-        self.app.repeat(500, self.on_enter)
+        self.app.repeat(500, self.exit_welcome_view)
+
+    def exit_welcome_view(self):
+        face_recognized = True
+        if face_recognized is True:
+            self.app.cancel(self.exit_welcome_view)
+            self.on_exit_view(PinView)
+        elif face_recognized is False:
+            self.app.cancel(self.exit_welcome_view)
+            self.on_exit_view(CantRecognizeView)
 
     def destroy(self):
         self.header_text.destroy()
@@ -21,13 +29,12 @@ class WelcomeView:
 class PinView:
     view_name = 'pin_view'
 
-    def __init__(self, application, history):
+    def __init__(self, application, on_exit_view = None):
         self.app = application
         self.header_text = Text(self.app, text="Welcome PERSON", size=40, font="Times New Roman", color="lightblue", grid=[])
         self.pin_text = Text(self.app, text="Enter your PIN to validate", size=30, font="Times New Roman", color="lightblue", grid=[])
         self.pin_input = TextBox(self.app, command=self.check_pin)
         self.pin_error = Text(self.app, text="", visible=False, color="red")
-        self.history = history
     
     def check_pin(self, enteredKey):
         currentPinEntry = self.pin_input.value + enteredKey
@@ -48,49 +55,46 @@ class PinView:
 class CantRecognizeView:
     view_name = 'cant_recognize_view'
 
-    def __init__(self, application, history):
+    def __init__(self, application, on_exit_view = None):
         self.app = application
         self.header_text = Text(self.app, text="Can't Recognize You!", size=40, font="Times New Roman", color="lightblue", grid=[])
-        self.history = history
 
     def destroy(self):
         self.header_text.destroy()
 
-class BikeCheckApp:
-    def __init__(self, app, history):
-        self.app = app(title="BikeCheck")
-        self.back_button = PushButton(self.app, command=self.on_back, text="Back")
-        self.current_view = WelcomeView(self.app, self.exit_welcome_view, history)
-        self.history = history
+class Navigation:
+    views = { 'welcome_view': WelcomeView, 'pin_view': PinView, 'cant_recognize_view': CantRecognizeView }
+    def __init__(self, app, initial_view):
+        self.app = app
+        self.back_button = PushButton(self.app, command=self.on_back, text="Back", enabled=False)
+        self.history = []
+        self.current_view = initial_view(self.app, self.on_exit_view)
 
     def on_back(self):
-        if (len(self.history) > 1):
+        if (len(self.history) > 0):
             last_view = self.history.pop()
-            if (last_view == CantRecognizeView.view_name):
-                self.current_view.destroy()
-                self.current_view = CantRecognizeView(self.app, self.history)
-            if (last_view == PinView.view_name):
-                self.current_view.destroy()
-                self.current_view = PinView(self.app, self.history)
-        elif(len(self.history) == 1):
-            self.current_view.destroy()
-            self.current_view = WelcomeView(self.app, self.exit_welcome_view, self.history)
+            self.update_current_view(self.views[last_view](self.app, self.on_exit_view), True)
+            if (len(self.history) == 0):
+                self.back_button.disable()
 
-    def exit_welcome_view(self):
-        face_recognized = True
-        if face_recognized is True:
-            self.app.cancel(self.exit_welcome_view)
-            self.current_view.destroy()
-            self.current_view = PinView(self.app, self.history)
-        elif face_recognized is False:
-            self.app.cancel(self.exit_welcome_view)
-            self.current_view.destroy()
-            self.current_view = CantRecognizeView(self.app, self.history)
+    def on_exit_view(self, next_view):
+        self.update_current_view(next_view(self.app, self.on_exit_view))
+
+    def update_current_view(self, next_view, isBack = False):
+        if isBack is False:
+            self.history.append(self.current_view.view_name)
+        self.current_view.destroy()
+        self.current_view = next_view
+        self.back_button.enable()
+
+class BikeCheckApp:
+    def __init__(self, app):
+        self.app = app(title="BikeCheck")
+        self.nav = Navigation(app=self.app, initial_view=WelcomeView)
 
     def start(self):
         self.app.display()
 
-history = ['welcome_view']
-app = BikeCheckApp(App, history)
+app = BikeCheckApp(App)
 app.start()
 
