f21753874d0d0a1d731f56d2497dbf8c901a24c8
Cristina Lewitton <cristina.lewitton@walmart.com>
2018-08-07 18:26:05 -0500

    Added face module draft

--- gui/components/welcome_component.py
+++ gui/components/welcome_component.py
@@ -1,5 +1,7 @@
 from framework.gzframe_component import GZFrameComponent
-from framework.gzframe_elements import GZFrameButton, GZFrameText, GZFrameContainer
+from framework.gzframe_elements import GZFrameButton, GZFrameText, GZFrameContainer, GZFramePicture
+from interfaces import face
+
 
 class WelcomeComponent(GZFrameComponent):
     def __init__(self, element_name, state={}, element_props={}):
@@ -10,16 +12,24 @@ class WelcomeComponent(GZFrameComponent):
 
     def recognize_face(self):
         self.gzframe.element_by_name('enter_button').disable()
-        self.gzframe.element_by_name('welcome').repeat(500, self.on_face_recognition)
+        # self.gzframe.element_by_name('welcome').repeat(500, self.on_face_recognition)
+        face = Face()
 
-    def on_face_recognition(self):
-        face_recognized = True
-        if face_recognized is True:
-            self.gzframe.element_by_name('welcome').cancel(self.on_face_recognition)
-            self.go_to_route('enter_pin')
+        face.grab_frame()
+
+        user_detected, user = face.detect_users()
+
+        if user_detected:
+            face.draw_user(user)
+
+            self.render(PictureElement(face.face))
+
+            # self.gzframe.element_by_name('welcome').cancel(self.on_face_recognition)
+            self.go_to_route('enter_one_more')
+
         elif face_recognized is False:
             self.gzframe.element_by_name('welcome').cancel(self.on_face_recognition)
-            self.go_to_route('no_recognition')
+            self.go_to_route('enter_both_rfid_and_pin')
 
     def render(self, props, state):
         return [
---
+++ gui/interfaces/face.py
@@ -0,0 +1,38 @@
+import cv2
+import sys
+import os
+import requests
+
+class Face(object):
+
+    def __init__(self, image_path="face.jpg"):
+        self.frame = None
+        self.face = None
+        self.image_path = image_path
+
+
+    def grab_frame(self):
+        frame_detected = False
+        while not frame_detected:
+            # Capture frame-by-frame
+            ret, frame = video_capture.read()
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame_detected = True
+            self.frame = frame
+            cv2.imwrite(self.image_path, self.frame)
+
+            video_capture.release()
+
+    def detect_users(self):
+
+        r = requests.post(face_url, data=open(self.image_path, 'rb').read(), params={}, headers={})
+        return (r.status_code  == 200, r.json())
+
+
+    def draw_user(self, user):
+        #user == {'dimensions':[2,3,100,100]}
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in user['dimensions']:
+            cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            self.face = self.frame[y:y+h, x:x+w]
