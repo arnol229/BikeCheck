9f3de403a8b0a1585c0de64cb44efdcc85faa412
Christian Monsegue <c0m01h9@homeoffice.wal-mart.com>
2018-08-06 22:02:16 -0500

    Nested component testing

---
+++ gui/components/bike_list_component.py
@@ -0,0 +1,21 @@
+from framework.gzframe_component import GZFrameComponent
+from framework.gzframe_elements import GZFrameButton, GZFrameText, GZFrameContainer
+
+class BikeListComponent(GZFrameComponent):
+    def __init__(self, element_name, props={}, state={}, element_props={}):
+        super().__init__(element_name, props=props, state=state, element_props=element_props)
+
+    def create_list(self, current_list):
+        bike_list = []
+        for x in range(0, len(current_list)):
+            num = str(x)
+            bike_list.append(
+                GZFrameContainer(element_name=("bike_item_group_" + num), children=[
+                    GZFrameText(element_name=("bike_group_" + num), element_props={"text":current_list[x]['text'], "size":20, "font":"Times New Roman", "color":"black", "grid": [0,0]}),
+                    GZFrameText(element_name=("bike_item_status_" + num), element_props={"text":current_list[x]['availability'], "size":20, "font":"Times New Roman", "color":"black", "grid": [1,0]})
+                ], element_props={'layout': 'grid'})
+            )
+        return bike_list
+
+    def render(self, props, state):
+        return self.create_list(props['bike_list'])
--- gui/components/dashboard_component.py
+++ gui/components/dashboard_component.py
@@ -1,9 +1,10 @@
 from framework.gzframe_component import GZFrameComponent
 from framework.gzframe_elements import GZFrameButton, GZFrameText, GZFrameContainer
+from components.bike_list_component import BikeListComponent
 
 class DashboardComponent(GZFrameComponent):
-    def __init__(self, component_name, props={}, state={}, element_props={}):
-        super().__init__(component_name, props=props, state=state, element_props=element_props)
+    def __init__(self, element_name, props={}, state={}, element_props={}):
+        super().__init__(element_name, props=props, state=state, element_props=element_props)
 
     def on_init(self):
         self.reset_history()
@@ -16,22 +17,20 @@ class DashboardComponent(GZFrameComponent):
         self.go_to_route('maintenance')
 
     def render(self, props, state):
-        def get_available_bikes():
-            available_bikes = []
-            for x in range(0, 5):
-                num = str(x)
-                available_bikes.append(
-                    GZFrameContainer(element_name=("bike_item_group_" + num), children=[
-                        GZFrameText(element_name=("bike_group_" + num), element_props={"text":("Bike #" + str(x + 1)), "size":20, "font":"Times New Roman", "color":"black", "grid": [0,0]}),
-                        GZFrameText(element_name=("bike_item_status_" + num), element_props={"text":"Available", "size":20, "font":"Times New Roman", "color":"black", "grid": [1,0]})
-                    ], element_props={'layout': 'grid'})
-                )
-            return available_bikes
+        def get_bike_list():
+            return {
+                'bike_list': [{
+                    'text': 'Mountain Bike',
+                    'availability': 'Available'
+                }]
+            }
+
+        bike_list = get_bike_list()
 
         return [
             GZFrameButton(element_name="logout_button", element_props={"text":"Log Out", "command": self.logout}),
             GZFrameText(element_name="header_text", element_props={"text":"Available Bikes", "size":30, "font":"Times New Roman", "color":"lightblue"}),
-            GZFrameContainer(element_name="bike_list_group", children=get_available_bikes()),
+            BikeListComponent(element_name="bike_list", props=bike_list),
             GZFrameButton(element_name="checkout_button", element_props={"text":"Check Out"}),
             GZFrameButton(element_name="maintenance_button", element_props={"text":"Maintenance", "command": self.to_maintenance})
         ]
