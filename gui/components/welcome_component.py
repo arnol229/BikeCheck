13f5e29795c3a2fa2d3f3c26de8762dc2c2719ca
Christian Monsegue <c0m01h9@homeoffice.wal-mart.com>
2018-08-13 14:17:41 -0500

    Fixed app crashing when no face detected

--- gui/components/welcome_component.py
+++ gui/components/welcome_component.py
@@ -8,6 +8,9 @@ import time
 class WelcomeComponent(GZFrameComponent):
     def __init__(self, element_name, state={}, props={}):
         super().__init__(element_name, state=state, props=props, width=800)
+        self.user_detected = None
+        self.user = None
+        self.retries = 0
 
     def gz_on_init(self):
         self.reset_history()
@@ -15,30 +18,41 @@ class WelcomeComponent(GZFrameComponent):
 
     def on_frame(self):
         if (self.state.face.frame is not None):
+            self.user_detected, self.user = self.state.face.detect_users()
             self.element.cancel(self.on_frame)
-            user_detected, user = self.state.face.detect_users()
-            if type(user) == list:
-                user = user[0]
+            self.element.repeat(2000, self.on_user_detected)
 
-            if user_detected:
+    def on_user_detected(self):
+        if self.user_detected is not None:
+            self.element.cancel(self.on_user_detected)
+            if type(self.user) == list and len(self.user) > 0:
+                user = self.user[0]
                 cropped_user_img = self.state.face.find_user(user)
                 im = Image.fromarray(cropped_user_img)
                 self.state.update_state({'auth': {'face': im}})
                 pic = GZFramePicture(element_name="user_picture", props={'image': self.state.auth['face']}, height=200, width=200)
                 text = GZFrameText(element_name="picture_text", props={"text":"Welcome {}!".format(user.get('name', 'Associate')), "size":50, "font":"Times New Roman", "color":"black"})
-                self.gzframe.view.destroy('header_text')
-                self.gzframe.view.destroy('enter_button')
+                self.gzframe.view.destroy('welcome_group')
                 self.gzframe.view.render(text)
                 self.gzframe.view.render(pic)
                 # pic.element.show()
-            self.gzframe.app.after(4000, self.go_to_verify)
+                self.gzframe.app.after(2000, self.go_to_verify)
+            else:
+                self.user_detected = None
+                self.retries += 1
+                self.recognize_face()
 
     def recognize_face(self):
-        self.gzframe.element_by_name('enter_button').disable()
-        if not isinstance(self.state.face, Face):
-            self.state.face = Face()
-        self.state.face.grab_frame()
-        self.element.repeat(200, self.on_frame)
+        print(self.retries)
+        if self.retries < 5:
+            self.gzframe.element_by_name('enter_button').disable()
+            if not isinstance(self.state.face, Face):
+                self.state.face = Face()
+            self.state.face.grab_frame()
+            self.element.repeat(500, self.on_frame)
+        else:
+            self.retries = 0
+            self.gzframe.element_by_name('enter_button').enable()
         
 
     def go_to_verify(self):
@@ -46,6 +60,8 @@ class WelcomeComponent(GZFrameComponent):
 
     def render(self, state):
         return [
-            GZFrameText(element_name="header_text", props={"text":"Welcome to BikeCheck", "size":40, "font":"Times New Roman", "color":"lightblue"}),
-            GZFrameButton(element_name="enter_button", props={"text":"Enter", "command": self.recognize_face}),
+            GZFrameContainer(element_name='welcome_group', children=[
+                GZFrameText(element_name="header_text", props={"text":"Welcome to BikeCheck", "size":40, "font":"Times New Roman", "color":"lightblue"}),
+                GZFrameButton(element_name="enter_button", props={"text":"Enter", "command": self.recognize_face}),
+            ])
         ]
